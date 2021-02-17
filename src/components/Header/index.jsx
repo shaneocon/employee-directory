1e67ffc76d53919c70e2d0f048d719bf115d9bda
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-16 21:38:51 -0800

    added Headercomponent

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className = "text-center">
+        Employee Directory
+      </h1>
+      <p className = "text-center">
+        Click on column headers to filer by heading or use the search box to narrow your results. 
+      </p>
+    </header>
+  );
+};
+
+export default Header;
