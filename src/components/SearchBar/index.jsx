e445f4435c01e29b2745d31bc12a92bd2d82d1fb
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-16 21:30:44 -0800

    added searchBar

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+const SearchBar = (props) => {
+
+  return (
+    <nav className = "navbar navbar-light bg-light justify-content-center">
+      <form className = "form-inline m-2" onSubmit = {props.handleFormSubmit}>
+        <input
+          className = "form-control"
+          value = {props.value}
+          name = "search"
+          onChange = {props.handleInputChange}
+          type = "search"
+          placeholder = "Search Here"
+        />
+      </form>
+    </nav>
+  );
+};
+
+export default SearchBar;
